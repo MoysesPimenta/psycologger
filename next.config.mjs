cdad2ecac8bb6c6c0cbe6eda3e2c748e355fff00
Moyses <benmuyalmoshe@gmail.com>
2026-04-07 19:46:25 -0300

    fix(build): wire next-intl plugin into next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 import { withSentryConfig } from "@sentry/nextjs";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -57,7 +60,7 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
+export default withSentryConfig(withNextIntl(nextConfig), {
   silent: true,
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
